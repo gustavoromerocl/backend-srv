694a69ff0e164137a16d700dcc0d853df0f7c348
gustavoromerocl <gandresrp@gmail.com>
2024-11-17 22:32:28 -0300

    feat: create reting model

---
+++ src/main/java/com/duocuc/backend_srv/model/Rating.java
@@ -0,0 +1,56 @@
+package com.duocuc.backend_srv.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "rating")
+public class Rating {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "recipe_id", nullable = false)
+  private Recipe recipe;
+
+  @Column(nullable = false)
+  private Integer value; // Calificación del 1 al 5
+
+  // Getters y setters
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public User getUser() {
+    return user;
+  }
+
+  public void setUser(User user) {
+    this.user = user;
+  }
+
+  public Recipe getRecipe() {
+    return recipe;
+  }
+
+  public void setRecipe(Recipe recipe) {
+    this.recipe = recipe;
+  }
+
+  public Integer getValue() {
+    return value;
+  }
+
+  public void setValue(Integer value) {
+    this.value = value;
+  }
+}
