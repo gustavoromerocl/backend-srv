f3dd3450710523b28ccee0f436bce9c994b3b24f
gustavoromerocl <gandresrp@gmail.com>
2024-11-17 22:35:39 -0300

    refactor: implement comment and reting service

--- src/main/java/com/duocuc/backend_srv/service/RecipeService.java
+++ src/main/java/com/duocuc/backend_srv/service/RecipeService.java
@@ -1,7 +1,11 @@
 package com.duocuc.backend_srv.service;
 
+import com.duocuc.backend_srv.model.Comment;
 import com.duocuc.backend_srv.model.Recipe;
 import com.duocuc.backend_srv.repository.RecipeRepository;
+
+import jakarta.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -41,4 +45,17 @@ public class RecipeService {
         }
         recipeRepository.deleteById(id);
     }
+
+    public List<Comment> getCommentsForRecipe(Long recipeId) {
+        Recipe recipe = recipeRepository.findById(recipeId)
+                .orElseThrow(() -> new EntityNotFoundException("Recipe not found"));
+        return recipe.getComments();
+    }
+
+    public Double getAverageRatingForRecipe(Long recipeId) {
+        Recipe recipe = recipeRepository.findById(recipeId)
+                .orElseThrow(() -> new EntityNotFoundException("Recipe not found"));
+        return recipe.getAverageRating();
+    }
+
 }
